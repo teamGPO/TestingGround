e80949f3c10ace38e96c19b1b36254ecbfa54cba
Justin <justinlovelessx@gmail.com>
2016-05-19 17:19:21 -0500

    added lots of things

---
+++ TestingGround/Assets/projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectile : MonoBehaviour {
+	public GameObject proj;
+	public float maxLiveTime;
+	// Use this for initialization
+	void Start () {
+		Destroy (proj, maxLiveTime);
+	}
+
+}
